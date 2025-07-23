4bb8e1defa1fcd939feab644cb9778b05f726af0
Roger Gerecke <roger.gerecke@gmail.com>
2025-07-23 19:24:31 +0200

    Remove redundant parentis add myspeed function

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
-# DryRoom Controller with Arduino Nano ATmega328 (New Bootloader)
-https://docs.platformio.org/en/latest/boards/atmelavr/nanoatmega328new.html
-- Arduino Nano
-- Atmega328P 16Mhz
-- USB-C
+# Arduino Nano ATmega328P (New Bootloader) Dry & Humidity control with relay and fan
 
-## Forum Post
-[https://forum.arduino.cc/t/kleine-klimasteuerung-fragen-bevor-es-durchbrennt/1393747](https://forum.arduino.cc/t/kleine-klimasteuerung-fragen-bevor-es-durchbrennt/1393747)
+---
+Electricity can kill or seriously injure you. Work involving electricity should only be carried out by qualified personnel. This project is not an instruction manual. I assume no liability for damages.
+---
+
+
+In my case use Clion with platform.io for deploying this project below based on this setup.
+https://docs.platformio.org/en/latest/boards/atmelavr/nanoatmega328new.html
+
+Is not the right bootload selected given upload failure and notice warning in console output.
+
+- Arduino Nano with ATmega328P
+- 16Mhz Tact rate
+- USB-C port
+- Tow case fans with PWM
+  - Case fan on chanel 9
+  - Case fan on chanel 10
+
 
 ![Trocknung_ArduinoNano_Steckplatine.png](fritzing/Trocknung_ArduinoNano_Steckplatine.png)
 
-# Specification PWM Case Fan
-### PWM Signal specification 25 kHz best patrice case fan
+# Generate 25kHz PWM signal with Arduino
+Generate the right 25kHz signal for **tow** output pins is no problem with the Arduino Nano.
+For this we update the internal hardware TIMER1 so we had the other timer for millis() or delay().  First we stop the interrupts, config the timer and ports and activate the interrupts.
+
+#### Tow pins need tow conection
+
+`TCCR1A |= (1 << COM1A1) | (1 << COM1B1);`
+
+
 
 ```
 void setup() {
@@ -19,41 +37,54 @@ void setup() {
     /*
      *  DEFINITION TIMER1 FOR ATMEL ATmega328P
      *  https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
-     */
+     *  https://www.mikrocontroller.net/articles/AVR-Tutorial:_Timer
+    */
 
-    // RESET
+    // RESET  Clear Timer/Counter control registers
     TCNT1 = 0;
     TCCR1A = 0;
-    TCCR1B = 0; // defined timer & register to 0
+    TCCR1B = 0;
 
-    // TOP
+    // Set TOP value for 25kHz frequency (ICR1)
     ICR1 = 320; // Set TOP limiter to 320 >>> update OCR1A at TOP
 
     // REGISTER DEFINITION
-    TCCR1B |= (1 << WGM13); // The phase correct Pulse Width Modulation or phase correct PWM mode (WGM13:0 = 1
     TCCR1A |= (1 << WGM11); // PWM, Phase Correct
-    TCCR1A |= (1 << COM1A1); // Clear OC1A/OC1B on Compare match when down counting set output to low level
+    TCCR1B |= (1 << WGM13); // The phase correct Pulse Width Modulation or phase correct PWM mode (WGM13:0 = 1
     TCCR1B |= (1 << CS10); // clkI/O/1 (No prescaling)
 
+    // Need COM1A1 and COM1B1 for tow pin output . Clear OC1A/OC1B on compare match
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1); // Clear OC1A/OC1B on Compare match when down counting set output to low level
+
+    // Set Arduino Nano pins to output with Data Direction Registers B (Arduino Pin 9 is PB1), (Arduino Pin 10 is PB2) is a part of Port B
+    DDRB |= (1 << 1) | (1 << 2); // set as OUTPUT
+
+    // Set RELAY pin PD5 is a part off Port D
+    DDRD |= (1 << 5);   // set as OUTPUT
+    PORTD &= ~(1 << 5); // set to LOW
+
+    // end
     sei(); // allow interrupts
 ```
 
-## Attention
-Minimum fan PWM signal in my case 5% duty cycle. Use Paired Timer TC1 on pin 3 & 11
+## Attention*
+Minimum fan PWM signal in my case 5% duty cycle. Use Paired Timer TC1 on pin 9 & 10. *Standard relays aren't sufficient. To switch high power, proper relays are needed.
 
 
 ## Hardware to test the dry room controller
 
 Important is we use Timer1 >> and the located output to OC1A, OC1B by Arduino Nano with ATmega328P. 
 
-| Pin | Method         | Hard.                             | More.    |
-|-----|----------------|-----------------------------------|----------|
-| 10  | analogWrite()  | Top FAN                           | PWM      |
-| 9   | analogWrite()  | Bottom FAN                        | PWM      |
-| 18  | readBytes()    | SHT 45                            | SDA 0x44 |
-| 19  | readBytes()    | SHT 45                            | SCL 0x44 |
-| 18  | readBytes()    | Display 16*2                      | SDA 0x27 |
-| 19  | readBytes()    | Display 16*2                      | SCL 0x27 |
-| 14  | readAnalog()   | FAN-Tacho Top                     |          |
-| 15  | readAnalog()   | FAN-Tacho Bottom                  |          |
-| 5   | digitalWrite() | Relay SPDT, 30 A, SLA-05VDC-SL-C  |          |
+| Pin | Method         | Hard.                                 | More.      |
+|-----|----------------|---------------------------------------|------------|
+| 10  | analogWrite()  | Top FAN                               | PWM        |
+| 9   | analogWrite()  | Bottom FAN                            | PWM        |
+| 18  | readBytes()    | SHT 45                                | SDA 0x44   |
+| 19  | readBytes()    | SHT 45                                | SCL 0x44   |
+| 18  | readBytes()    | Display 16*2                          | SDA 0x27   |
+| 19  | readBytes()    | Display 16*2                          | SCL 0x27   |
+| 14  | readAnalog()   | FAN-Tacho Top                         |            |
+| 15  | readAnalog()   | FAN-Tacho Bottom                      |            |
+| 5   | digitalWrite() | Relay SPDT, **30 A**, SLA-05VDC-SL-C  | attention* |
+
+*Standard relays aren't sufficient. To switch high power, proper relays are needed.
--- src/main.cpp
+++ src/main.cpp
@@ -7,57 +7,68 @@
 // Cable green = SCL PIN_19  **A5
 // Cable yellow = SDA PIN_18 **A4
 // Cable black = GND PIN_GND
-#include <Wire.h>
 #include <ArtronShop_SHT45.h>
-ArtronShop_SHT45 sensor(&Wire, 0x44); // default is right (&Wire, 0x44) SHT45-AD1B => I2C address 0x44
+#include <Wire.h>
+ArtronShop_SHT45 sensor(&Wire,
+                        0x44); // default is right (&Wire, 0x44) SHT45-AD1B => I2C address 0x44
 float temperature;
 float humidity;
-constexpr float MAX_TEMPERATURE = 18.0;
-constexpr float MAX_HUMIDITY = 58.0;
-constexpr float MED_HUMIDITY = 55.0;
+constexpr float MIN_TEMPERATURE = 17.0; // cooling stops
+constexpr float MAX_TEMPERATURE = 19.0; // cooling starts
+constexpr float MAX_HUMIDITY = 58.0;    // Circulation max speed
+constexpr float MIN_HUMIDITY = 54.0;    // Below this value Circulation FAN_MINIMUM_DUTY_CYCLE
 
 // >>> ### --- DISPLAY --- ### >>>
 // Cable green = SCL PIN_19  **A5
 // Cable yellow = SDA PIN_18 **A4
-#include <LiquidCrystal_I2C.h>       //Include lib
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for 16 chars and 2 line display
+#include <LiquidCrystal_I2C.h> //Include lib
+LiquidCrystal_I2C lcd(0x27, 16,
+                      2); // set the LCD address to 0x27 for 16 chars and 2 line display
 
 // >>> ### --- COOLING_COMPRESSOR RELAY --- ### >>>
 constexpr int COOLING_COMPRESSOR_PIN = 5; // Signal to SPDT, 30 A
 
-// >>> ### --- PWM CASE FAN 25 kHz --- ### >>>
-struct Pin {
+/*
+ * Configuration for the PWM fan signal and the circulation setup.
+ * Bind the pins
+ */
+struct Pin
+{
     uint8_t pwmPin;
 };
 
-constexpr Pin FAN_PWM_PIN_A{10};// 10
-constexpr Pin FAN_PWM_PIN_B{9}; //9
-constexpr int DEFAULT_SPEED = 5; // The lowest PWM Duty Cycle the fank can look in datasheet for your fan
+constexpr Pin FAN_PWM_PIN_TOP_FAN{10};         // 10
+constexpr Pin FAN_PWM_PIN_BOTTOM_FAN{9};       // 9
+constexpr uint8_t FAN_MINIMUM_DUTY_CYCLE = 6;  // The lowest PWM Duty Cycle the fank can look
+                                               // in datasheet for your fan
+constexpr uint8_t FAN_MAXIMAL_DUTY_CYCLE = 50; // Maximal Fan Speed
+constexpr uint8_t CIRCULATION_SPREAD = 10;     // Circulation spread between pwm groups top fan bottom fan
 bool fanOn = false;
 
-
+// INT CONF
 unsigned long previousMillis = 0;
-unsigned long interval = 10000; // 10 second
-bool checkup = false;
+unsigned long temp_interval = 30000; // 30 second temp_interval for checkup relay off?
+unsigned long dry_interval = 180000; // 3 Minutes 180000ms
 
 // debug function uncomment for debug
-//#define DEBUG_PWM true
+bool checkup = false;
+// #define DEBUG_PWM true
 #ifdef DEBUG_PWM
 long i = 0;
 long timer_cycles = 0;
 long Frequency = 0;
 
-
-bool pwmSignalDebug(const long i) {
+bool pwmSignalDebug(const long i)
+{
     // arduino nano 16MHz
     Serial.println("PWM Signal Debug Output Nr: " + String(i));
     Serial.println("#############################");
 
     // show pin
     Serial.print("PWM Connected to pin: ");
-    Serial.print(FAN_PWM_PIN_A.pwmPin);
+    Serial.print(FAN_PWM_PIN_TOP_FAN.pwmPin);
     Serial.print(" and ");
-    Serial.println(FAN_PWM_PIN_B.pwmPin);
+    Serial.println(FAN_PWM_PIN_BOTTOM_FAN.pwmPin);
     Serial.println("----------------------");
 
     // test calculation of duty cycle
@@ -103,14 +114,20 @@ bool pwmSignalDebug(const long i) {
 }
 #endif
 
-void setup() {
+void setup()
+{
     cli(); // disable interrupts
 
     /*
+     *  FIND MORE INFORMATION HOW IT WORKS
+     *
      *  DEFINITION TIMER1 FOR ATMEL ATmega328P
      *  https://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
      *  https://www.mikrocontroller.net/articles/AVR-Tutorial:_Timer
-    */
+     *
+     *
+     *  CHANGE TIMER1 SETTINGS FOR 25kHz SIGNAL CALCULATION
+     */
 
     // RESET  Clear Timer/Counter control registers
     TCNT1 = 0;
@@ -121,14 +138,26 @@ void setup() {
     ICR1 = 320; // Set TOP limiter to 320 >>> update OCR1A at TOP
 
     // REGISTER DEFINITION
-    TCCR1A |= (1 << WGM11); // PWM, Phase Correct
-    TCCR1B |= (1 << WGM13); // The phase correct Pulse Width Modulation or phase correct PWM mode (WGM13:0 = 1
-    TCCR1B |= (1 << CS10); // clkI/O/1 (No prescaling)
+    TCCR1A |= 1 << WGM11; // PWM, Phase Correct
+    TCCR1B |= 1 << WGM13; // The phase correct Pulse Width Modulation or phase
+                          // correct PWM mode (WGM13:0 = 1
+    TCCR1B |= 1 << CS10;  // clkI/O/1 (No prescaling)
 
-    // Need COM1A1 and COM1B1 for tow pin output
-    TCCR1A |= (1 << COM1A1) | (1 << COM1B1); // Clear OC1A/OC1B on Compare match when down counting set output to low level
+    // Need COM1A1 and COM1B1 for tow pin output . Clear OC1A/OC1B on compare
+    // match
+    TCCR1A |= 1 << COM1A1 | 1 << COM1B1;
+    // Clear OC1A/OC1B on Compare match when down counting set output to low
+    // level
 
+    // Set Arduino Nano pins to output with Data Direction Registers B (Arduino
+    // Pin 9 is PB1), (Arduino Pin 10 is PB2) is a part of Port B
+    DDRB |= 1 << 1 | 1 << 2; // set as OUTPUT
 
+    // Set RELAY pin PD5 is a part off Port D
+    DDRD |= 1 << 5;     // set as OUTPUT
+    PORTD &= ~(1 << 5); // set to LOW
+
+    // end
     sei(); // allow interrupts
 
     // inti serial
@@ -138,21 +167,16 @@ void setup() {
     lcd.init();
     lcd.backlight();
 
-    // Relay init the pin set it to "OUTPUT"
+    /*// Relay init the pin set it to "OUTPUT"
     pinMode(COOLING_COMPRESSOR_PIN, OUTPUT);
     digitalWrite(COOLING_COMPRESSOR_PIN, LOW);
-    delay(15);
-
-    // pwm
-    pinMode(FAN_PWM_PIN_A.pwmPin, OUTPUT);
-    delay(15);
-    pinMode(FAN_PWM_PIN_B.pwmPin, OUTPUT);
-    delay(15);
+    delay(15);*/
 
     // init wire chanel check SHT45 connected
 #ifndef DEBUG_PWM
     Wire.begin();
-    while (!sensor.begin()) {
+    while (!sensor.begin())
+    {
         Serial.println("SHT45 notfound!");
         // add error to display
         lcd.print("SHT45 notfound!");
@@ -162,55 +186,118 @@ void setup() {
 }
 
 // overwrite default analogWrite function for pwm fan pins
-void analogWrite(const Pin &pin, const uint8_t percent) {
-    if (pin.pwmPin == FAN_PWM_PIN_A.pwmPin) { OCR1A = map(percent, 0, 100, 0, ICR1);} // ICR1 320
-    if (pin.pwmPin == FAN_PWM_PIN_B.pwmPin) { OCR1B = map(percent, 0, 100, 0, ICR1);} // ICR1 320
+void analogWrite(const Pin &pin, const uint8_t percent)
+{
+    if (pin.pwmPin == FAN_PWM_PIN_TOP_FAN.pwmPin)
+    {
+        OCR1A = map(percent, 0, 100, 0, ICR1);
+    } // ICR1 320
+    if (pin.pwmPin == FAN_PWM_PIN_BOTTOM_FAN.pwmPin)
+    {
+        OCR1B = map(percent, 0, 100, 0, ICR1);
+    } // ICR1 320
+}
+
+void circulation(const uint8_t percent)
+{
+    if (percent <= FAN_MINIMUM_DUTY_CYCLE)
+    { // stop fan
+        analogWrite(FAN_PWM_PIN_TOP_FAN, 0);
+        analogWrite(FAN_PWM_PIN_BOTTOM_FAN, 0);
+        Serial.println("Stop fan circulation.");
+        return;
+    }
+
+    if (percent - CIRCULATION_SPREAD <= FAN_MINIMUM_DUTY_CYCLE)
+    { // minimum circulation
+        analogWrite(FAN_PWM_PIN_TOP_FAN, FAN_MINIMUM_DUTY_CYCLE);
+        analogWrite(FAN_PWM_PIN_BOTTOM_FAN, FAN_MINIMUM_DUTY_CYCLE);
+        Serial.println("Fan circulation spread value to big: Fan run default speed.");
+        return;
+    }
+
+    // calculated circulation
+    analogWrite(FAN_PWM_PIN_TOP_FAN, percent - CIRCULATION_SPREAD);
+    analogWrite(FAN_PWM_PIN_BOTTOM_FAN, percent);
+
+    Serial.print("Fan run ");
+    Serial.print(percent);
+    Serial.println("%");
+}
+
+// calculate the percent speed for todo validate types and resolution
+uint8_t mySpeed(const float humi)
+{
+    if (humi >= MAX_HUMIDITY)
+    {
+        return FAN_MAXIMAL_DUTY_CYCLE;
+    }
+    if (humi <= MIN_HUMIDITY)
+    {
+        return FAN_MINIMUM_DUTY_CYCLE;
+    }
+
+    constexpr float h_range = MAX_HUMIDITY - MIN_HUMIDITY;                   // 4
+    constexpr int s_range = FAN_MAXIMAL_DUTY_CYCLE - FAN_MINIMUM_DUTY_CYCLE; // 45
+
+    constexpr float step = s_range / h_range;
+
+    const auto speed = static_cast<uint8_t>(humi - MIN_HUMIDITY * step);
+
+    return speed; // 12
 }
 
 // system boot test
-bool startUpCheck() {
+bool startUpCheck()
+{
     // display message
     lcd.setCursor(0, 0);
     lcd.print("Startup Check");
-    analogWrite(FAN_PWM_PIN_A, 100);
-    analogWrite(FAN_PWM_PIN_B, 100);
-    Serial.println("Fan run 100%");
+    analogWrite(FAN_PWM_PIN_TOP_FAN, 100);
+    analogWrite(FAN_PWM_PIN_BOTTOM_FAN, 10);
+    Serial.println("Fan run 100% and 10%");
     delay(20000);
-    analogWrite(FAN_PWM_PIN_A, 50);
-    analogWrite(FAN_PWM_PIN_B, 50);
+    analogWrite(FAN_PWM_PIN_TOP_FAN, 10);
+    analogWrite(FAN_PWM_PIN_BOTTOM_FAN, 100);
+    Serial.println("Fan run 10% and 100%");
+    delay(20000);
+    analogWrite(FAN_PWM_PIN_TOP_FAN, 50);
+    analogWrite(FAN_PWM_PIN_BOTTOM_FAN, 50);
     Serial.println("Fan run 50%");
     delay(10000);
-    analogWrite(FAN_PWM_PIN_A, 30);
-    analogWrite(FAN_PWM_PIN_B, 30);
+    analogWrite(FAN_PWM_PIN_TOP_FAN, 30);
+    analogWrite(FAN_PWM_PIN_BOTTOM_FAN, 30);
     Serial.println("Fan run 30%");
     delay(10000);
-    analogWrite(FAN_PWM_PIN_A, 5);
-    analogWrite(FAN_PWM_PIN_B, 5);
+    analogWrite(FAN_PWM_PIN_TOP_FAN, 5);
+    analogWrite(FAN_PWM_PIN_BOTTOM_FAN, 5);
     Serial.println("Fan run 5%");
     delay(10000);
-    analogWrite(FAN_PWM_PIN_A, 100);
-    analogWrite(FAN_PWM_PIN_B, 100);
-    Serial.println("Last run 100%");
+    analogWrite(FAN_PWM_PIN_TOP_FAN, 100);
+    analogWrite(FAN_PWM_PIN_BOTTOM_FAN, 100);
+    Serial.println("Fast run 100%");
     delay(10000);
     lcd.clear();
     return true;
-
 }
 
-void loop() {
+void loop()
+{
 #ifdef DEBUG_PWM
     i++;
     pwmSignalDebug(i);
-#endif
 
     // Boot up fan check
-    if (checkup == false) {
+    if (checkup == false)
+    {
         checkup = startUpCheck();
     }
+#endif
 
 #ifndef DEBUG_PWM
     // if the sensor connected
-    if (sensor.measure()) {
+    if (sensor.measure())
+    {
         // serial debug message
         Serial.print("Temperature: ");
         Serial.print(sensor.temperature(), 1);
@@ -226,12 +313,17 @@ void loop() {
 
         // HANDLE TEMPERATURE
         //  is HIGH
-        if (temperature >= MAX_TEMPERATURE) { digitalWrite(COOLING_COMPRESSOR_PIN, HIGH); }
+        if (temperature >= MAX_TEMPERATURE)
+        {
+            digitalWrite(COOLING_COMPRESSOR_PIN, HIGH);
+        }
 
-        if (currentMillis - previousMillis >= interval) {
+        if (currentMillis - previousMillis >= temp_interval)
+        {
             lcd.clear();
 
-            if (temperature >= MAX_TEMPERATURE) {
+            if (temperature >= MAX_TEMPERATURE)
+            {
                 // start compressor
                 digitalWrite(COOLING_COMPRESSOR_PIN, HIGH);
                 // display message
@@ -239,13 +331,17 @@ void loop() {
                 lcd.print("Kuehlung ON");
                 lcd.setCursor(0, 1);
                 lcd.print(String("Temperatur: ") + String(temperature));
+                delay(1000);
+                lcd.clear();
                 // Fan circulation on
-                if (!fanOn) {
+                if (!fanOn)
+                {
                     fanOn = true;
-                    analogWrite(FAN_PWM_PIN_A, DEFAULT_SPEED);
-                    analogWrite(FAN_PWM_PIN_B, DEFAULT_SPEED);
+                    circulation(FAN_MINIMUM_DUTY_CYCLE);
                 }
-            } else {
+            }
+            else
+            {
                 // stop compressor
                 digitalWrite(COOLING_COMPRESSOR_PIN, LOW);
                 // display message
@@ -253,49 +349,48 @@ void loop() {
                 lcd.print("Kuehlung OFF");
                 lcd.setCursor(0, 1);
                 lcd.print(String("Temperatur: ") + String(temperature));
+                delay(1000);
+                lcd.clear();
             }
         }
 
         // set new time
         previousMillis = currentMillis;
 
-        // HANDLE HUMIDITY
-        if (humidity >= MAX_HUMIDITY) {
-            // MAX_HUMIDITY: Fast fan circulation it is real wet
+        // it is wet
+        if (humidity >= MIN_HUMIDITY)
+        {
             fanOn = true;
-            analogWrite(FAN_PWM_PIN_A, 10);
-            analogWrite(FAN_PWM_PIN_B, 10);
-            // display message
+            const long speed = mySpeed(humidity);
+            circulation(speed);
             lcd.setCursor(0, 0);
             lcd.print(String("Humidity: ") + String(humidity));
             lcd.setCursor(0, 1);
-            lcd.print("Fast Fan! 50%");
-        } else if (humidity > MED_HUMIDITY) {
-            // MED_HUMIDITY: Moderate fan circulation
-            fanOn = true;
-            analogWrite(FAN_PWM_PIN_A, 40);
-            analogWrite(FAN_PWM_PIN_B, 50);
-            // display message
-            lcd.setCursor(0, 0);
-            lcd.print(String("Humidity: ") + String(humidity));
-            lcd.setCursor(0, 1);
-            lcd.print("Lower Fan!.");
-        } else {
-            // DEFAULT: Slow fan circulation
-            fanOn = true;
-            analogWrite(FAN_PWM_PIN_A, DEFAULT_SPEED);
-            analogWrite(FAN_PWM_PIN_B, DEFAULT_SPEED);
-            // display message
-            lcd.setCursor(0, 0);
-            lcd.print(String("Humidity: ") + String(humidity));
-            lcd.setCursor(0, 1);
-            lcd.print("Slow Fan!.");
+            lcd.print("Fan Speed:" + String(speed));
+            delay(5000);
+            lcd.clear();
         }
-    } else {
+
+        // it is too dry interval circulation
+        if (humidity < MIN_HUMIDITY and currentMillis - previousMillis >= dry_interval)
+        {
+            circulation(FAN_MINIMUM_DUTY_CYCLE);
+            lcd.setCursor(0, 0);
+            lcd.print(String("Humidity: ") + String(humidity));
+            lcd.setCursor(0, 1);
+            lcd.print("Fan Speed:" + String(FAN_MINIMUM_DUTY_CYCLE));
+            delay(30000); // 30sec
+            lcd.clear();
+            fanOn = false;
+            analogWrite(FAN_PWM_PIN_TOP_FAN, 0);
+            analogWrite(FAN_PWM_PIN_BOTTOM_FAN, 0);
+        }
+    }
+    else
+    {
         // sensor connection error run fan by default low
         fanOn = true;
-        analogWrite(FAN_PWM_PIN_A, DEFAULT_SPEED);
-        analogWrite(FAN_PWM_PIN_B, DEFAULT_SPEED);
+        circulation(FAN_MINIMUM_DUTY_CYCLE);
         // print out error message
         Serial.println("SHT45 read error");
         lcd.setCursor(0, 0);
